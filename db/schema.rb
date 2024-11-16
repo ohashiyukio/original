8a9735d2c73049304310256fea5b08d0ee464844
ohashiyukio <ohashiyukio222@gmail.com>
2024-11-16 22:09:23 +0900

    質問相談投稿修理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_13_030051) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_16_130415) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_13_030051) do
     t.bigint "pet_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["pet_id"], name: "index_questions_on_pet_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
@@ -106,4 +106,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_13_030051) do
   add_foreign_key "diaries", "pets"
   add_foreign_key "pets", "users"
   add_foreign_key "questions", "pets"
+  add_foreign_key "questions", "users"
 end
